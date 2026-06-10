b2a735d051218619dfda21fc8d5165c866290017
Miro-Stojanovic97 <stojanovic.miro97@gmail.com>
2026-06-10 12:36:59 -0500

    adding background and skeleton

--- miro_io/src/app/page.tsx
+++ miro_io/src/app/page.tsx
@@ -1,65 +1,80 @@
 import Image from "next/image";
+import React from "react";
+import Wave from 'react-wavify';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+    <div className="grid grid-cols-5 grid-rows-1 min-h-screen">
+        <main className="col-start-2 col-span-3 bg-[url(/backdropv2.svg)] bg-center bg-contain bg-no-repeat grid grid-cols-5 grid-rows-5">
+          <div className="col-start-2 row-start-2">
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className="rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
+              About Me
+            </a>
+          </div>
+          <div className="col-start-4 row-start-2">
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className="rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+              Experience
+            </a>
+          </div>
+          <div className="col-start-2 row-start-4">
+            <a
+              className="rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact Me
+            </a>
+          </div>
+          <div className="col-start-4 row-start-4 order-last px-5"> 
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/boat.svg"
+              alt="boat"
+              width={60}
+              height={60}
+              className=""
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          </div>
+          <div className="col-start-1 row-start-4 col-span-5 justify-items-center">
+            <Wave fill="url(#gradient)"
+                  paused={false}
+                  style={{ display: 'flex-1', width: '81.5%', justifyContent: 'center', alignItems: 'center', marginLeft: '-0.5%' }}
+                  options={{
+                    height: 26,
+                    amplitude: 25,
+                    speed: 0.2,
+                    points: 0.5
+                  }}
+            >
+              <defs>
+                <linearGradient id="gradient" gradientTransform="rotate(90)">
+                  <stop offset="10%"  stopColor="#c5e5ff" />
+                  <stop offset="90%" stopColor="#4dabf7" />
+                </linearGradient>
+              </defs>
+            </Wave>
+          </div>
+          <div className="col-start-2 row-start-5 col-span-3">
+            <p className="text-3xl text-white text-center font-calibri">
+              hi, i'm miro :) explore to learn more about me!
+            </p>
+          </div>
+          <div className="col-start-2 row-start-5 col-span-3 py-15">
+            <p className="text-white text-center font-italic">
+              [This website is a work in progress. TODO: Responsive design, Adding on-click content modals]
+            </p>
+          </div>
+        </main>
     </div>
   );
 }
